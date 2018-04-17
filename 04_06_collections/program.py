50a69f257a1b2f79b78c71d7193bc198fef3252b
Yang Dai <yangdai713@aim.com>
2018-04-17 17:05:55 -0400

    finished day 7 practice

--- 04_06_collections/program.py
+++
@@ -1,9 +0,0 @@
-"""
-A Pomodoro Technique.
-How it works:
-    1. Set a pomodoro timer (default is 25 min).
-    2. When the timer ends, a short break timer (default is 5 min) kicks in.
-    3. Go to step 1.
-    4. After four pomodoro cycles (step 1 to 3), take a longer break (default
-       is 30 min).
-"""
