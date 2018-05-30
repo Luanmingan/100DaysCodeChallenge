80b0a755d554a5e11caebb9f86c4c40c5e876d3e
superyang713 <yangdai713@aim.com>
2018-05-30 09:57:43 -0400

    working on day1

--- 55_57_uplink/day1/program.py
+++ 55_57_uplink/day1/program.py
@@ -1,4 +1,3 @@
-import uplink
 from blog_client import BlogClient
 
 
@@ -18,26 +17,38 @@ def main():
 def read_post():
     svc = BlogClient()
     r = svc.all_entries()
-    r.raise_for_status()
 
     posts = r.json()
     for i, p in enumerate(posts, 1):
         print(" {}. [{} views] {}".format(
             i, p.get('view_count'), p.get('title')
         ))
+    print()
 
     selected = int(input('Which number to view? '))
     selected_id = posts[selected-1].get('id')
     selected_post = svc.entry_by_id(selected_id)
 
     r = svc.entry_by_id(selected_id)
-    r.raise_for_status()
 
     selected_post = r.json()
+    print("Details for select_post: {}".format(selected_post.get('id')))
+    print("Title: " + selected_post.get('title'))
+    print("Written: " + selected_post.get('published'))
 
 
 def write_post():
-    pass
+    svc = BlogClient()
+
+    title = input("Title: ")
+    content = input("body contents: ")
+    view_count = int(input("view count (int): "))
+
+    r = svc.create_new_entry(title, content, view_count)
+
+    print('\n')
+    print("Created new post succesfully: {}".format(r.json().get('id')))
+    print('\n')
 
 
 if __name__ == '__main__':
