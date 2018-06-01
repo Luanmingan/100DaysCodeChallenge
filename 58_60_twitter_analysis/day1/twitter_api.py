bbea2ffb1ed65ee376f7dd750a1d9c94800e93d8
Yang Dai <yangdai713@aim.com>
2018-06-01 12:27:34 -0400

    finished day1 for twitter analysis

--- 58_60_twitter_analysis/day1/twitter_api.py
+++ 58_60_twitter_analysis/day1/twitter_api.py
@@ -1,17 +1,9 @@
-from collections import namedtuple, Counter
+from collections import namedtuple
 import os
-import re
-
-import matplotlib.pyplot as plt
-import numpy as np
-from PIL import Image
 import tweepy
-from wordcloud import WordCloud, STOPWORDS
 
 
 Tweet = namedtuple('Tweet', 'id text created likes rts')
-
-TWITTER_ACCOUNT = 'pybites'
 
 TWITTER_KEY = os.environ['TWITTER_KEY']
 TWITTER_SECRET = os.environ['TWITTER_SECRET']
@@ -19,13 +11,39 @@ TWITTER_ACCESS_TOKEN = os.environ['TWITTER_ACCESS_TOKEN']
 TWITTER_ACCESS_SECRET = os.environ['TWITTER_ACCESS_SECRET']
 
 
-auth = tweepy.OAuthHandler(TWITTER_KEY, TWITTER_SECRET)
-auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
-api = tweepy.API(auth)
+class Twitter:
+    def __init__(self, account_name):
+        self.account_name = account_name
+        self.api = None
 
+    def connect(self):
+        print("Connecting...")
+        auth = tweepy.OAuthHandler(TWITTER_KEY, TWITTER_SECRET)
+        auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
+        print("Connected to twitter API...")
+        self.api = tweepy.API(auth)
 
-def get_tweets():
-    for tw in tweepy.Cursor(api.user_timeline, screen_name=TWITTER_ACCOUNT,
-                            exclude_replies=False, include_rts=True).items():
-        yield Tweet(tw.id, tw.text, tw.created_at, tw.favorite_count,
-                    tw.retweet_count)
+    def _get_tweets(self):
+        for tw in tweepy.Cursor(
+            self.api.user_timeline, screen_name=self.account_name,
+            exclude_replies=False, include_rts=True
+        ).items():
+            yield Tweet(tw.id,
+                        tw.text,
+                        tw.created_at,
+                        tw.favorite_count,
+                        tw.retweet_count)
+
+    def get_tweets(self, exclude_retweet=False):
+        tweets = self._get_tweets()
+        if not exclude_retweet:
+            yield from tweets
+        else:
+            return (tweet for tweet in tweets if not
+                    tweet.text.startswith('RT'))
+
+    def sort_by_popularity(self):
+        excl_rts = list(self.get_tweets(exclude_retweet=False))
+        sorted_tweets = sorted(excl_rts, key=lambda tw: (tw.likes + tw.rts)/2,
+                               reverse=True)
+        return sorted_tweets
