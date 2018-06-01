4e07c1bbfbb0724d5b51ba3731ab264df111356e
superyang713 <yangdai713@aim.com>
2018-05-31 23:14:36 -0400

    working on day57 day1

--- 58_60_twitter_analysis/day1/twitter_api.py
+++ 58_60_twitter_analysis/day1/twitter_api.py
@@ -10,3 +10,26 @@ from wordcloud import WordCloud, STOPWORDS
 
 
 Tweet = namedtuple('Tweet', 'id text created likes rts')
+
+TWITTER_ACCOUNT = 'pybites'
+
+TWITTER_KEY = os.environ['TWITTER_KEY']
+TWITTER_SECRET = os.environ['TWITTER_SECRET']
+TWITTER_ACCESS_TOKEN = os.environ['TWITTER_ACCESS_TOKEN']
+TWITTER_ACCESS_SECRET = os.environ['TWITTER_ACCESS_SECRET']
+
+
+auth = tweepy.OAuthHandler(TWITTER_KEY, TWITTER_SECRET)
+auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
+api = tweepy.API(auth)
+
+
+def get_tweets():
+    for tw in tweepy.Cursor(api.user_timeline, screen_name=TWITTER_ACCOUNT,
+                            exclude_replies=False, include_rts=True).items():
+        yield Tweet(tw.id, tw.text, tw.created_at, tw.favorite_count,
+                    tw.retweet_count)
+
+for tweet in get_tweets():
+    print(tweet)
+
