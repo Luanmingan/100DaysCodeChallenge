fd7031d61be7db75dfef0da34f4a686d373c306c
Yang Dai <yangdai713@aim.com>
2018-06-01 12:27:34 -0400

    finished day1 for twitter analysis

--- 58_60_twitter_analysis/day1/twitter_api.py
+++ 58_60_twitter_analysis/day1/twitter_api.py
@@ -1,6 +1,5 @@
-from collections import namedtuple, Counter
+from collections import namedtuple
 import os
-import re
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -29,3 +28,29 @@ def get_tweets():
                             exclude_replies=False, include_rts=True).items():
         yield Tweet(tw.id, tw.text, tw.created_at, tw.favorite_count,
                     tw.retweet_count)
+
+
+tweets = list(get_tweets())
+
+all_tweets_excl_rts_mentions = ' '.join(
+    [tw.text.lower() for tw in tweets if not tw.text.startswith('RT') and not
+     tw.text.startswith('@')]
+)
+
+pb_mask = np.array(Image.open("pybites.png"))
+stopwords = set(STOPWORDS)
+stopwords.add('co')
+stopwords.add('https')
+
+wc = WordCloud(background_color='white', max_words=2000, mask=pb_mask,
+               stopwords=stopwords)
+
+wc.generate(all_tweets_excl_rts_mentions)
+
+
+plt.figure(figsize=(30, 30))
+plt.imshow(wc, interpolation="bilinear")
+plt.margins(x=0, y=0)
+plt.axis("off")
+
+plt.show()
