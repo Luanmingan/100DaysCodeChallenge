c75b1529201e9b6c8d35317d42ed8b379b0f5c15
Yang Dai <yangdai713@aim.com>
2018-06-07 18:07:29 -0400

    add several plot methods for gender city province

--- 58_60_twitter_analysis/day2/program.py
+++ 58_60_twitter_analysis/day2/program.py
@@ -4,8 +4,9 @@ from wechat_api import Wechat
 def main():
     wechat = Wechat()
     wechat.login(auto=True)
+
     friends = wechat.get_friends()
-    print(friends)
+    [print(friend.nickname) for friend in friends]
 
 
 if __name__ == '__main__':
--- 58_60_twitter_analysis/day2/wechat_api.py
+++ 58_60_twitter_analysis/day2/wechat_api.py
@@ -1,11 +1,16 @@
 import itchat
-from collections import namedtuple
+from collections import namedtuple, Counter
+from matplotlib.font_manager import FontProperties
+import matplotlib.pyplot as plt
 
 
 Record = namedtuple(
     'Record',
     'nickname sex province city signature'
 )
+font_path = '/home/yang/.local/share/fonts/wqy-zenhei.ttc'
+prop = FontProperties(fname=font_path)
+plt.figure(figsize=(20, 20))
 
 
 class Wechat():
@@ -32,3 +37,60 @@ class Wechat():
             signature=friend.get('Signature')
         )
         return record
+
+    def plot_gender(self, show_unknown=False):
+        num_of_male = 0
+        num_of_female = 0
+        num_of_other = 0
+        for friend in self.get_friends():
+            if friend.sex == 1:
+                num_of_male += 1
+            elif friend.sex == 2:
+                num_of_female += 1
+            else:
+                num_of_other += 1
+        if show_unknown:
+            x = ['Male', 'Female', 'Other']
+            y = [num_of_male, num_of_female, num_of_other]
+        else:
+            x = ['Male', 'Female']
+            y = [num_of_male, num_of_female]
+        plt.bar(x, y)
+        plt.xlabel('Gender')
+        plt.ylabel('Counts')
+        plt.title('Gender Distribution')
+        plt.show()
+
+    def plot_province(self, n=10, show_unknown=False):
+        if show_unknown:
+            provinces = [record.province for record in self.get_friends()]
+        else:
+            provinces = [record.province for record in self.get_friends()
+                         if record.province]
+        counter = Counter(provinces)
+        top_provinces = counter.most_common(n)
+        x, y = zip(*top_provinces)
+
+        plt.bar(x, y)
+        plt.xticks(fontproperties=prop)
+        plt.xlabel('Province')
+        plt.ylabel('Counts')
+        plt.title('Province Distribution')
+        plt.show()
+
+    def plot_city(self, n=15, show_unknown=False):
+        if show_unknown:
+            cities = [record.city for record in self.get_friends()]
+        else:
+            cities = [record.city for record in self.get_friends()
+                      if record.city]
+        counter = Counter(cities)
+        top_cities = counter.most_common(n)
+        x, y = zip(*top_cities)
+
+        plt.bar(x, y)
+        plt.xticks(fontproperties=prop)
+        plt.xlabel('City')
+        plt.ylabel('Counts')
+        plt.title('City Distribution')
+        plt.show()
